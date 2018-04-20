70eed0d0972da93c7f6bf19cb8a4de6df1af7db4
mingwho <mh3833@nyu.edu>
2018-04-20 18:19:37 +0400

    added tags for filler videos in json database hoo ray

--- web-player/static/scripts/convert.py
+++ web-player/static/scripts/convert.py
@@ -1,19 +1,26 @@
+
+# This file updates the video type of videos.
+# A regular avatar video will have video-type regular
+# And a filler avatar video will have video-type filler
+
 import json
 
-f= open("all_characters.json", "r")
+f = open("all_characters.json", "r")
 
-fw= open('all_characters-new.json', 'w')
+fw = open('all_characters-new.json', 'w')
 
 resp = json.load(f)
 
-data= ' "language": "English" , '
+filler_list = [338, 339, 340, 341, 342, 1210, 1211, 1212, 1213, 1214, 2269, 2270, 2271, 2272, 2273, 2284, 2285, 2286,
+               2287, 2288, 3210, 3211, 3212, 3213, 3214]
 
+for i in range(0, len(resp["rows"]) - 1, 1):
+    if "character" in resp["rows"][i]["doc"].keys():
+        resp["rows"][i]["doc"]["video-type"] = "regular"
 
-
-for i in range (0, len(resp["rows"])-1, 1):
-    if("question" in resp["rows"][i]["doc"].keys()):
-        resp["rows"][i]["doc"]["language"]= "English"
+        if resp["rows"][i]["doc"]["index"] in filler_list:
+            resp["rows"][i]["doc"]["video-type"] = "filler"
 
 json.dump(resp, fw)
 
-#f.close()
+f.close()
