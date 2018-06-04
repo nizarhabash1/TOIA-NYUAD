17be3904bf690bd24b9fab906dc721bfab16e508
mingwho <mh3833@nyu.edu>
2018-06-04 19:02:11 +0400

    working on entry deletion, currently deleting js object entry but not writing to json file

--- web-recorder/public/main.js
+++ web-recorder/public/main.js
@@ -31,6 +31,8 @@ $.ajax({
 			});
 			//Clear out current data on the page if any
 			$('#questionContainer').html('');
+      console.log("your all data  is ")
+      console.log(allData);
 			var htmlString = makeHTML(allData);
 
 			// console.log(htmlString);
@@ -38,14 +40,14 @@ $.ajax({
 			//Bind events to each object
 			allData.forEach(function(d){
 				setDeleteEvent(d);
-				setUpdateEvent(d);
-				setSaveEvent(d);
-				setPlayEvent(d);
+				// setUpdateEvent(d);
+				// setSaveEvent(d);
+				// setPlayEvent(d);
 			});
 			/* Scroll to last saved video */
-			$('#questionContainer').animate({
-		        scrollTop: $(scroll_id).offset().top
-		    });
+			// $('#questionContainer').animate({
+		  //       scrollTop: $(scroll_id).offset().top
+		  //   });
 		    //updateCharacter();
 		}
 	});
@@ -55,19 +57,20 @@ $.ajax({
 function makeHTML(theData){
 
 	var htmlString = '<ul id="theDataList">';
-	theData.forEach(function(d){
-		htmlString += '<li id='+ d.doc.index + '>' + d.doc.index + '. ' + d.doc['english-question']
-		+ ' <br> ' + d.doc['english-answer'];
-		htmlString += '<br><button id=' + d.doc._rev + ' class="updateButton">UPDATE</button>';
-		htmlString += '<button id=' + d.doc._id + ' class="deleteButton">DELETE</button>';
+	theData.forEach(function(data_with_id_and_key){
+    d = data_with_id_and_key.doc;
+		htmlString += '<li id='+ d.index + '>' + d.index + '. ' + d['english-question']
+		+ ' <br> ' + d['english-answer'];
+		htmlString += '<br><button id=' + d._rev + ' class="updateButton">UPDATE</button>';
+		htmlString += '<button id=' + d._id + ' class="deleteButton">DELETE</button>';
 		// console.log("The video is " + d.video);
 		if(d.video!=""){
-			htmlString += '<button id=' + 'save_' + d.doc.index + ' class="saveButton" style="background-color:#e7e7e7">SAVE</button>';
-			htmlString += '<button id=' + 'play_' + d.doc.index + ' class="playButton" >PLAY</button>';
+			htmlString += '<button id=' + 'save_' + d.index + ' class="saveButton" style="background-color:#e7e7e7">SAVE</button>';
+			htmlString += '<button id=' + 'play_' + d.index + ' class="playButton" >PLAY</button>';
 		}
 		else{
-			htmlString += '<button id=' + 'save_' + d.doc.index + ' class="saveButton">SAVE</button>';
-			htmlString += '<button id=' + 'play_' + d.doc.index + ' class="playButton" style="display:none">PLAY</button>';
+			htmlString += '<button id=' + 'save_' + d.index + ' class="saveButton">SAVE</button>';
+			htmlString += '<button id=' + 'play_' + d.index + ' class="playButton" style="display:none">PLAY</button>';
 		}
 
 		// var blob_id = "blob_" + d.index;
@@ -78,49 +81,47 @@ function makeHTML(theData){
 	return htmlString;
 }
 
-function saveData(obj){
-  //TODO: replace with code to add entries to JSON on the go from browser
-}
+// function saveData(obj){
+//   //TODO: replace with code to add entries to JSON on the go from browser
+// }
+//
+//
+// function setSaveEvent(data){
+// 		var theID = '#save_' + data.doc.index;
+// 		scroll_id = '#save_' + data.doc.index;
+// 		$(theID).click(function(){
+// 			var theObj = _.find(allData, function(d){
+// 				return d.index == data.doc.index;
+// 			});
+// 			console.log("we are SAVING " + data.doc.index);
+// 			//Change a value
+// 			this_video_name= this_character + "_" + data.doc.index +
+//                             "_.mp4";
+// 			$("#save-to-disk").trigger('click');
+//       //TODO:Please add this line back with correct format
+// 			// theObj.video = this_video_name;
+// 			sendUpdateRequest(theObj);
+// 	});
+// }
 
-
-function setSaveEvent(data){
-		var theID = '#save_' + data.doc.index;
-		scroll_id = '#save_' + data.doc.index;
-		$(theID).click(function(){
-			var theObj = _.find(allData, function(d){
-				return d.index == data.doc.index;
-			});
-			console.log("we are SAVING " + data.doc.index);
-			//Change a value
-			this_video_name= this_character + "_" + data.doc.index +
-                            "_.mp4";
-			$("#save-to-disk").trigger('click');
-      //TODO:Please add this line back with correct format
-			// theObj.video = this_video_name;
-			sendUpdateRequest(theObj);
-
-
-	});
-}
-
-function setPlayEvent(data){
-		var theID = '#play_' + data.index;
-		$(theID).click(function(){
-			var theObj = _.find(allData, function(d){
-				return d.index == data.index;
-			});
-			console.log("we are PLAYING " + data.index);
-      //TODO: maybe you need to update the following video name
-			//Change a value
-			var play_video_name= this_character + "-videos/" + this_character+"_" + data.index +
-                            "_"+ theObj._id + ".mp4";
-            // var gumVideo = document.querySelector('video#gum');
-            // gumVideo.src = play_video_name;
-            // gumVideo.play();
-			recordingPlayer.src = play_video_name;
-			recordingPlayer.play();
-	});
-}
+// function setPlayEvent(data){
+// 		var theID = '#play_' + data.index;
+// 		$(theID).click(function(){
+// 			var theObj = _.find(allData, function(d){
+// 				return d.index == data.index;
+// 			});
+// 			console.log("we are PLAYING " + data.index);
+//       //TODO: maybe you need to update the following video name
+// 			//Change a value
+// 			var play_video_name= this_character + "-videos/" + this_character+"_" + data.index +
+//                             "_"+ theObj._id + ".mp4";
+//             // var gumVideo = document.querySelector('video#gum');
+//             // gumVideo.src = play_video_name;
+//             // gumVideo.play();
+// 			recordingPlayer.src = play_video_name;
+// 			recordingPlayer.play();
+// 	});
+// }
 
 // function updateCharacter(data){
 // 	for(var i = 1001; i < 1214; i++){
@@ -134,48 +135,58 @@ function setPlayEvent(data){
 // 	}
 // }
 
-
-function setUpdateEvent(data){
-		var theID = '#' + data._rev;
-		$(theID).click(function(){
-			var theObj = _.find(allData, function(d){
-				return d._rev == data._rev;
-			});
-			//Change a value
-			var promptVal = prompt('Enter a new answer to the question "' + theObj.question + '":');
-			if (!promptVal) return;
-			theObj.answer = promptVal;
-			sendUpdateRequest(theObj);
-	});
-}
-
-function sendUpdateRequest(obj){
-	$('#questionContainer').html('<div id="loading">Data is being updated...</div>');
-	console.log(obj);
-  //TODO: add replace entry in Json here
-}
+//
+// function setUpdateEvent(data){
+// 		var theID = '#' + data._rev;
+// 		$(theID).click(function(){
+// 			var theObj = _.find(allData, function(d){
+// 				return d._rev == data._rev;
+// 			});
+// 			//Change a value
+// 			var promptVal = prompt('Enter a new answer to the question "' + theObj.question + '":');
+// 			if (!promptVal) return;
+// 			theObj.answer = promptVal;
+// 			sendUpdateRequest(theObj);
+// 	});
+// }
+//
+// function sendUpdateRequest(obj){
+// 	$('#questionContainer').html('<div id="loading">Data is being updated...</div>');
+// 	console.log(obj);
+//   //TODO: add replace entry in Json here
+// }
 
 
 function setDeleteEvent(data){
-	var theID = '#' + data._id;
+	var theID = '#' + data.id;
+  // if the delete button is clicked
 	$(theID).click(function(){
-		var theObj = _.find(allData, function(d){
-			return d._id == data._id;
-		});
-		console.log(theObj);
-		sendDeleteRequest(theObj);
+
+
+    $.each(allData,function(i){
+      if(allData[i].id == data.id){
+        var conf = confirm("Are you sure you want to delete '" + allData[i].doc["english-question"] + " : " + allData[i].doc["english-answer"] + "' ?");
+        if (!conf) return;
+        allData.splice(i,1);
+        return false;
+      }
+    })
+    console.log("after deletion");
+    console.log(allData);
+
+    //TODO: rewrite the allData object into the pre-existing json file at a specified location
+
 	});
 }
 
-function sendDeleteRequest(obj){
-	console.log(obj);
-	//Make sure you want to delete
-	var conf = confirm("Are you sure you want to delete '" + obj.question + " : " + obj.answer + "' ?");
-	if (!conf) return;
-	//Proceed if confirm is true
-	$('#dataContainer').html('<div id="loading">Data is being deleted...</div>');
-  //TODO: fill in deleting entry in JSON file
-}
+// function sendDeleteRequest(obj){
+// 	//Make sure you want to delete
+// 	var conf = confirm("Are you sure you want to delete '" + obj.doc["english-question"] + " : " + obj.doc["english-answer"] + "' ?");
+// 	if (!conf) return;
+// 	//Proceed if confirm is true
+// 	$('#dataContainer').html('<div id="loading">Data is being deleted...</div>');
+//   //TODO: fill in deleting entry in JSON file
+// }
 
 $(document).ready(function(){
 
@@ -207,9 +218,5 @@ $(document).ready(function(){
 	        $('#new-question').val('');
 	        $('#new-answer').val('');
 	    }
-
     });
-
-
-
 });
